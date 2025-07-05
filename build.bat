41cd359c6a0645cf04d9816868ce426734765f67
GongSunFangYun <bilishenxds@163.com>
2025-07-06 00:25:12 +0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,3 @@
+pyinstaller MPlus.spec
+# 也可以使用：
+# pyinstaller --onedir --noconsole --icon=ICON.ico --add-data "./functions;functions" --add-data "ICON.ico;." --name MPlus --distpath dist --workpath build --hidden-import=PySide6 MarkPlus.py
