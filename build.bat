f5d53e0be77a138bb4d3839053e6aa7b0b2980f5
GongSunFangYun <bilishenxds@163.com>
2025-07-09 21:48:42 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+pip install pyinstaller
+
 pyinstaller MPlus.spec
 # 也可以使用：
 # pyinstaller --onedir --noconsole --icon=ICON.ico --add-data "./functions;functions" --add-data "ICON.ico;." --name MPlus --distpath dist --workpath build --hidden-import=PySide6 MarkPlus.py
